aa065acc297b5c93e856b5d217e5423286b155eb
Victor <victorma07work@gmail.com>
2023-08-08 14:23:31 -0700

    added server folder

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
