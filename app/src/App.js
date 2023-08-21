5a1388744c578b5811d5306e81d121911f0e616a
VictorMa <victorma07work@gmail.com>
2023-08-21 14:36:28 -0700

    added routes

--- app/src/App.js
+++ app/src/App.js
@@ -3,13 +3,24 @@ import "./App.css";
 import Login from "./Pages/Login/Login";
 import AboutUs from "./Pages/AboutUs/AboutUs";
 import Dashboard from "./Pages/Dashboard/Dashboard";
+import Profile from "./Pages/Profile/Profile";
+import Search from "./Pages/Search/Search";
+import Subscription from "./Pages/Subscription/Subscription";
+import Title from "./Pages/Title/Title";
+import Watchlist from "./Pages/Watchlist/Watchlist";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
-
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/title" element={<Title />} />
+        <Route path="/watchlist" element={<Watchlist />} />
         <Route path="/" element={<Navigate to={"/login"} />} />
       </Routes>
     </div>
