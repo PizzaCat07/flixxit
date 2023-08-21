a128246ef121dfd072b1db794184bf434256bc0a
VictorMa <victorma07work@gmail.com>
2023-08-21 14:26:21 -0700

    create pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Login from "./Pages/Login/Login";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
+
+        <Route path="/" element={<Navigate to={"/login"} />} />
       </Routes>
     </div>
   );
