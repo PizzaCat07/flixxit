29be01eadd77f20c7f7eb75753426144a878b923
Victor <victorma07work@gmail.com>
2023-08-22 14:48:48 -0700

    found working carosel moudle

--- app/src/Pages/Dashboard/Dashboard.js
+++ app/src/Pages/Dashboard/Dashboard.js
@@ -1,23 +1,61 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../shared/components/Header";
-import { getPopularMovie } from "../../shared/api/movieApi";
+import { getPopularMovie, getDiscoverMovie } from "../../shared/api/movieApi";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import AliceCarousel from "react-alice-carousel";
 
 const Dashboard = () => {
-  const [popular, setPopular] = useState([]);
+  const [popularMovie, setPopularMovie] = useState([]);
+  const [discoverMovie, setDiscoverMovie] = useState([]);
+
+  const imageBaseUrl = "https://image.tmdb.org/t/p/w92";
+
+  const responsive = {
+    0: { items: 1 },
+    568: { items: 8 },
+    1024: { items: 12 },
+  };
 
   useEffect(() => {
-    getPopularMovie(setPopular);
+    getPopularMovie(setPopularMovie);
+    getDiscoverMovie(setDiscoverMovie);
   }, []);
 
-  console.log(popular);
+  console.log("popular Movie:", popularMovie);
+  console.log("discover Movie: ", discoverMovie);
 
   return (
     <>
-      <Header />
       <div>
-        {popular.map((x) => {
-          return <div>{x.original_title}</div>;
-        })}
+        <Header />
+        <AliceCarousel
+          infinite
+          responsive={responsive}
+          controlsStrategy="alternate"
+          items={popularMovie.map((x) => {
+            return (
+              <div className="item">
+                <img src={imageBaseUrl + x.poster_path} />
+              </div>
+            );
+          })}
+        />
+        <div>
+          <AliceCarousel
+            infinite
+            responsive={responsive}
+            controlsStrategy="alternate"
+            items={discoverMovie.map((x) => {
+              return (
+                <div className="item">
+                  <img src={imageBaseUrl + x.poster_path} />
+                </div>
+              );
+            })}
+          />
+        </div>
       </div>
     </>
   );
--- app/src/shared/api/movieApi.js
+++ app/src/shared/api/movieApi.js
@@ -14,4 +14,18 @@ const getPopularMovie = async (setState) => {
     });
 };
 
-export { getPopularMovie };
+const getDiscoverMovie = async (setState) => {
+  await fetch("https://api.themoviedb.org/3/discover/movie", {
+    headers: {
+      Authorization: process.env.REACT_APP_API_TOKEN,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((json) => {
+      //console.log(json.results);
+      const data = json.results;
+      setState(data);
+    });
+};
+
+export { getPopularMovie, getDiscoverMovie };
