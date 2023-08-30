aa17e85d128053f1ff9b4b7b0f39e1bf7735676b
Victor <victorma07work@gmail.com>
2023-08-30 14:05:42 -0700

    post watchedRecently on server

--- server/apis/rating/rating.js
+++ server/apis/rating/rating.js
@@ -15,7 +15,7 @@ const ratingRouter = Router();
 ratingRouter.get("/rating", (req, res) => {
   const id = req.headers.id;
   const email = req.headers.email;
-  console.log("get rating");
+
   getFilteredDocuments("rating", { id }).then((x) => {
     if (x.length > 0) {
       res.send(x);
--- server/apis/recentWatched/recentWatched.js
+++ server/apis/recentWatched/recentWatched.js
@@ -10,4 +10,41 @@ import { ObjectId, MongoClient } from "mongodb";
 
 const recentWatchedRouter = Router();
 
+recentWatchedRouter.get("/profile", (req, res) => {
+  const email = req.headers.email;
+
+  getFilteredDocuments("recentWatched", { email }).then((x) => {
+    res.json(x);
+  });
+});
+
+recentWatchedRouter.post("/profile", (req, res) => {
+  const id = req.headers.id;
+  const email = req.headers.email;
+  const { type, poster_path, genres } = req.body;
+  const data = {
+    id: id,
+    email: email,
+    type: type,
+    poster_path: poster_path,
+    genres: genres,
+  };
+
+  getFilteredDocuments("recentWatched", {
+    $and: [{ id }, { email }],
+  }).then((x) => {
+    if (x.length > 0) {
+      res.send({
+        exists: true,
+      });
+    } else {
+      insertDocument("recentWatched", data).then((x) => {
+        res.send({
+          success: true,
+        });
+      });
+    }
+  });
+});
+
 export default recentWatchedRouter;
